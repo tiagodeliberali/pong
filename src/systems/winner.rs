94d58971a1d34125374814006a1ea1416cec572e
Tiago Deliberali <tiago.deliberali@gmail.com>
2020-05-23 19:58:46 -0300

    added score

--- src/systems/winner.rs
+++ src/systems/winner.rs
@@ -2,28 +2,52 @@ use amethyst::{
     core::transform::Transform,
     core::SystemDesc,
     derive::SystemDesc,
-    ecs::prelude::{Join, System, SystemData, World, WriteStorage},
+    ecs::prelude::{Join, ReadExpect, System, SystemData, World, Write, WriteStorage},
+    ui::UiText,
 };
 
-use crate::pong::{Ball, ARENA_WIDTH};
+use crate::pong::{Ball, ScoreBoard, ScoreText, ARENA_WIDTH};
 
 #[derive(SystemDesc)]
 pub struct WinnerSystem;
 
 impl<'s> System<'s> for WinnerSystem {
-    type SystemData = (WriteStorage<'s, Ball>, WriteStorage<'s, Transform>);
+    type SystemData = (
+        WriteStorage<'s, Ball>,
+        WriteStorage<'s, Transform>,
+        WriteStorage<'s, UiText>,
+        Write<'s, ScoreBoard>,
+        ReadExpect<'s, ScoreText>,
+    );
 
-    fn run(&mut self, (mut balls, mut locals): Self::SystemData) {
+    fn run(&mut self, (
+        mut balls,
+        mut locals,
+        mut ui_text,
+        mut scores,
+        score_text
+    ): Self::SystemData) {
         for (ball, transform) in (&mut balls, &mut locals).join() {
             let ball_x = transform.translation().x;
 
             let did_hit = if ball_x <= ball.radius {
                 // Right player scored on the left side.
-                println!("Player 2 Scores!");
+                // We top the score at 999 to avoid text overlap.
+                scores.score_right = (scores.score_right + 1)
+                    .min(999);
+
+                if let Some(text) = ui_text.get_mut(score_text.p2_score) {
+                    text.text = scores.score_right.to_string();
+                }
                 true
             } else if ball_x >= ARENA_WIDTH - ball.radius {
                 // Left player scored on the right side.
-                println!("Player 1 Scores!");
+                // We top the score at 999 to avoid text overlap.
+                scores.score_left = (scores.score_left + 1)
+                    .min(999);
+                if let Some(text) = ui_text.get_mut(score_text.p1_score) {
+                    text.text = scores.score_left.to_string();
+                }
                 true
             } else {
                 false
@@ -32,6 +56,12 @@ impl<'s> System<'s> for WinnerSystem {
             if did_hit {
                 ball.velocity[0] = -ball.velocity[0]; // Reverse Direction
                 transform.set_translation_x(ARENA_WIDTH / 2.0); // Reset Position
+
+                // Print the scoreboard.
+                println!(
+                    "Score: | {:^3} | {:^3} |",
+                    scores.score_left, scores.score_right
+                );
             }
         }
     }
